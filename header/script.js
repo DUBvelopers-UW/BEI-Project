a98448bdb8e22d7794e0b7f76bacbfd47d7684c6
Nicolas Hitosis <nicolashitosis@Nicolass-MacBook-Pro.local>
2022-05-26 11:34:01 -0700

    Finished CSS for search bar

--- header/script.js
+++ header/script.js
@@ -2,6 +2,12 @@ const suggestionsTemplate = document.querySelector("[data-suggestions-template]"
 const suggestionsContainer = document.querySelector("[data-suggestions-container]");
 const searchInput = document.querySelector("[data-search]");
 
+
+
+
+
+
+
 let users = []; // Array of users
 
 searchInput.addEventListener("input", (e) => {
@@ -9,7 +15,12 @@ searchInput.addEventListener("input", (e) => {
     users.forEach(user => {
         const isVisible = user.name.toLowerCase().includes(value); // Checks if user.name has the search in it
         user.element.classList.toggle("show", isVisible);
-        if (isVisible && value.length > 0) {
+
+        user.element.classList.onclick(function(){
+            alert("test");
+        });
+
+        if (value.length > 0) {
             suggestionsContainer.style.display = "block";
         } else {
             suggestionsContainer.style.display = "none";
@@ -19,6 +30,7 @@ searchInput.addEventListener("input", (e) => {
 
 // Gets users from API and stores each one in users array
 fetch("https://jsonplaceholder.typicode.com/users")
+
     .then(res => res.json())
     .then(data => {
         users = data.map(user => {
@@ -29,3 +41,33 @@ fetch("https://jsonplaceholder.typicode.com/users")
             return { name: user.name, element: card }
         })
     })
+
+
+
+
+/* Lunr.js */
+/*
+var documents = [{
+    "name": "Lunr",
+    "text": "Like Solr, but much smaller, and not as bright.bright bright"
+}, {
+    "name": "React",
+    "text": "A JavaScript library for building user interfaces."
+}, {
+    "name": "Lodash",
+    "text": "A modern JavaScript utility library delivering modularity, performance & extras."
+}]
+
+var idx = lunr(function () {
+    this.ref('name')
+    this.field('text')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+})
+
+let results = idx.search("bright")
+console.log('Results: ', results.length);
+
+console.log('Sanity') */
